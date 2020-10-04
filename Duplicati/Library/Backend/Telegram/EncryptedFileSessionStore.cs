5f9c52dc98eb2bf09bd74b1106e8af339b6a73ba
Karen Martikyan <karen.martikyan@digitain.com>
2020-10-04 20:23:01 +0400

    Fixed exposing phone number/identity

--- Duplicati/Library/Backend/Telegram/EncryptedFileSessionStore.cs
+++ Duplicati/Library/Backend/Telegram/EncryptedFileSessionStore.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using TLSharp.Core;
 
 namespace Duplicati.Library.Backend
@@ -45,17 +47,18 @@ namespace Duplicati.Library.Backend
         private string GetSessionFilePath(string userId)
         {
             userId = userId.TrimStart('+');
+            var sha = GetShortSha(userId);
 
             var appData = Environment.SpecialFolder.LocalApplicationData;
-            var sessionFilePath = Path.Combine(Environment.GetFolderPath(appData), $"{userId}.dat");
+            var sessionFilePath = Path.Combine(Environment.GetFolderPath(appData), nameof(Duplicati), $"tg_{sha}.dat");
 
             return sessionFilePath;
         }
 
         private static void WriteToEncryptedStorage(byte[] bytesToWrite, string pass, string path)
         {
-            using (var file = File.Open(path, FileMode.Create, FileAccess.Write))
             using (var sessionMs = new MemoryStream(bytesToWrite))
+            using (var file = File.Open(path, FileMode.Create, FileAccess.Write))
             {
                 SharpAESCrypt.SharpAESCrypt.Encrypt(pass, sessionMs, file);
             }
@@ -68,11 +71,24 @@ namespace Duplicati.Library.Backend
                 return null;
             }
 
-            using (var file = File.Open(path, FileMode.Open, FileAccess.Read))
             using (var sessionMs = new MemoryStream())
+            using (var file = File.Open(path, FileMode.Open, FileAccess.Read))
             {
                 SharpAESCrypt.SharpAESCrypt.Decrypt(pass, file, sessionMs);
                 return sessionMs.ToArray();
+            }
+        }
+
+        private static string GetShortSha(string input)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            using (var sha = SHA256.Create())
+            {
+                var longShaBytes = sha.ComputeHash(inputBytes);
+                var longSha = BitConverter.ToString(longShaBytes).Replace("-", string.Empty);
+                var result = longSha.Substring(0, 16);
+
+                return result;
             }
         }
     }
